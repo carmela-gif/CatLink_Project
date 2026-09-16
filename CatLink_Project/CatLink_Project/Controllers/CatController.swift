49ef22d0645a70b5d74fbb79d5cd9ebcdffc43a1
Carmela <medenillakarminaanne@gmail.com>
2026-09-16 10:33:01 +0800

    Add colorPattern property to Cat model

--- CatLink_Project/CatLink_Project/Controllers/CatController.swift
+++ CatLink_Project/CatLink_Project/Controllers/CatController.swift
@@ -7,16 +7,18 @@
 
 import Foundation
 internal import Combine
-
+ 
 @MainActor
 final class CatController: ObservableObject {
     @Published var cats: [Cat] = [
         Cat(name: "Luna", breed: "British Shorthair", age: "1 Year, 2 Months",
-            weight: "4.2 kg", gender: "Female", healthInformation: "Healthy"),
+            weight: "4.2 kg", gender: "Female", healthInformation: "Healthy",
+            colorPattern: "Lilac-Cream"),
         Cat(name: "Mochi", breed: "Ragdoll Kitten", age: "3 Months",
-            weight: "2.1 kg", gender: "Male", healthInformation: "Healthy")
+            weight: "2.1 kg", gender: "Male", healthInformation: "Healthy",
+            colorPattern: "Blue Point")
     ]
-
+ 
     func addCat(_ cat: Cat) {
         cats.append(cat)
     }
