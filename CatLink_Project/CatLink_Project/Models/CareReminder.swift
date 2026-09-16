392aab73b6ea786faea6e885b102ed26d2a1a075
Carmela <medenillakarminaanne@gmail.com>
2026-09-16 10:30:32 +0800

    Add ReminderTag enum and update CareReminder struct
    Added ReminderTag enum with associated colors and updated CareReminder struct to include a tag and completion status.

--- CatLink_Project/CatLink_Project/Models/CareReminder.swift
+++ CatLink_Project/CatLink_Project/Models/CareReminder.swift
@@ -5,11 +5,27 @@
 //  Created by Mac-LAB on 9/8/26.
 //
 
-import Foundation
+import SwiftUI
 
+enum ReminderTag: String {
+    case feeding, medication, vet, grooming
+ 
+    var color: Color {
+        switch self {
+        case .feeding: return Theme.accentYellow
+        case .medication: return .blue
+        case .vet: return Theme.accentPurple
+        case .grooming: return .gray
+        }
+    }
+}
+ 
 struct CareReminder: Identifiable {
     let id = UUID()
     var title: String
     var description: String
     var date: String
+    var tag: ReminderTag = .feeding
+    var isCompleted: Bool = false
 }
+ 
